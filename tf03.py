f52b56d3b9ac2007a27222e467937549b481b94c
Michael Haberler <git@mah.priv.at>
2022-07-25 21:58:00 +0200

    fix rounding

--- tf03.py
+++ tf03.py
@@ -207,7 +207,7 @@ class InputChunkProtocol(asyncio.Protocol):
                         {
                             "values": [
                                 {"path": "tf03.outOfRange", "value": False},
-                                {"path": "tf03.altitude", "value": distance*0.01},
+                                {"path": "tf03.altitude", "value": round(distance*0.01, 2)},
                                 {"path": "tf03.errored", "value": errored},
                                 # {"path": "tf03.frames", "value": frames},
                                 {"path": "tf03.connected", "value": True},
